3d496179898999b4013d401013262ac139907feb
Nikita <admiralc@ya.ru>
2022-03-06 10:59:57 +0300

    Задача 2

--- Задача 2/Program.cs
+++ Задача 2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите номер переменной X первой точки => ");
+string inputA = Console.ReadLine();
+int A = int.Parse(inputA);
+Console.Write("Введите номер переменной Y первой точки => ");
+string inputB = Console.ReadLine();
+int B = int.Parse(inputB);
+Console.Write("Введите номер переменной X второй точки => ");
+string inputC = Console.ReadLine();
+int C = int.Parse(inputC);
+Console.Write("Введите номер переменной Y второй точки => ");
+string inputD = Console.ReadLine();
+int D = int.Parse(inputD);
+double answer = Math.Sqrt((A - C) * (A - C) + (B - D) * (B - D));
+Console.WriteLine(answer);
+
+
+
+
+
+
